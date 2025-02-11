4526e0406ebf2bd7947ccc233b12b858e5ce9787
France Huon <france.huon@lonestone.io>
2025-02-11 12:26:23 +0100

    :recycle: updated create in game service

--- apps/api/src/modules/game/game.service.ts
+++ apps/api/src/modules/game/game.service.ts
@@ -1,6 +1,5 @@
 import { EntityManager } from '@mikro-orm/sqlite'
 import { Injectable } from '@nestjs/common'
-import { CreateGameDto, GameDto } from '@shifumi/dtos'
 import { Game } from '../../entities/game.entity.js'
 import { Player } from '../../entities/player.entity.js'
 
@@ -8,26 +7,24 @@ import { Player } from '../../entities/player.entity.js'
 export class GameService {
   constructor(private readonly em: EntityManager) {}
 
-  async create(createGameDto: CreateGameDto): Promise<Pick<GameDto, 'id' | 'playerTwo'>> {
-    return await this.em.transactional(async (em) => {
-      const { playerTwoId } = createGameDto
-      const Computer = 1
-      const playerOne = await em.findOneOrFail(Player, { id: Computer })
-      const playerTwo = await em.findOneOrFail(Player, { id: playerTwoId })
+  async create(playerTwoId: number): Promise<Game> {
+    let playerOne = await this.em.findOne(Player, { id: 1 });
+    if(!playerOne) {
+      playerOne = await this.em.create(Player, { id: 1, name: 'Computer' });
+      await this.em.persistAndFlush(playerOne)
+    }
 
-      const game = em.create(Game, {
-        playerOne,
-        playerTwo,
-        createdAt: new Date(),
-        updatedAt: null,
-      })
-      await em.persistAndFlush(game)
-      return { id: game.id, playerTwo: game.playerTwo.id }
-    })
-  }
+  const playerTwo = await this.em.findOneOrFail(Player, { id: playerTwoId })
 
-  async getOne(id: number) {
-    const game = await this.em.findOne(Game, { id })
-    return game
+  const game = this.em.create(Game, {
+    createdAt: new Date(),
+    updatedAt: null,
+    playerOne,
+    playerTwo
+   })
+
+  await this.em.persistAndFlush(game)
+  return game
+
   }
 }
