659681f5b9bebca0c74f2d835606d7a472e34993
France Huon <france.huon@lonestone.io>
2025-02-24 11:48:39 +0100

    :hammer: updated game controller and service

--- apps/api/src/modules/game/game.controller.ts
+++ apps/api/src/modules/game/game.controller.ts
@@ -1,20 +1,21 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common'
 import { GameService } from './game.service.js'
-import { Game } from '../../entities/game.entity.js'
+import { GameDto } from '@shifumi/dtos'
 
 @Controller('games')
 export class GameController {
   constructor(private gameService: GameService) {}
 
   @Post()
-  async create(@Body() body: { playerTwoName: string }): Promise<Game> {
+  async create(@Body() body: { playerTwoName: string }): Promise<GameDto> {
     const newGame = await this.gameService.create(body.playerTwoName)
     return newGame
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: number): Promise<Game | null> {
-    return this.gameService.getOne(id)
+  async findOne(@Param('id') id: number): Promise<GameDto | null> {
+    const game = this.gameService.getOne(id)
+    return game
   }
 
 }
--- apps/api/src/modules/game/game.service.ts
+++ apps/api/src/modules/game/game.service.ts
@@ -1,22 +1,24 @@
 import { EntityManager } from '@mikro-orm/sqlite'
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
 import { Game } from '../../entities/game.entity.js'
 import { Player } from '../../entities/player.entity.js'
+import { GameDto } from '@shifumi/dtos';
 
 @Injectable()
 export class GameService {
   constructor(private readonly em: EntityManager) {}
 
-  async create(playerTwoName: string): Promise<Game> {
+  async create(playerTwoName: string): Promise<GameDto> {
+    // Pour le moment playerOne est toujours Computer
     let playerOne = await this.em.findOne(Player, { name: 'Computer' });
     if(!playerOne) {
-      playerOne = await this.em.create(Player, { name: 'Computer' });
+      playerOne = this.em.create(Player, { name: 'Computer' });
       await this.em.persistAndFlush(playerOne)
     }
 
     let playerTwo = await this.em.findOne(Player, { name: playerTwoName });
-    if(!playerTwo) {
-      playerTwo = await this.em.create(Player, { name: playerTwoName });
+    if (!playerTwo) {
+      playerTwo = this.em.create(Player, { name: playerTwoName });
       await this.em.persistAndFlush(playerTwo)
     }
 
@@ -28,10 +30,25 @@ export class GameService {
     })
 
     await this.em.persistAndFlush(game)
-    return game
+    return {
+      id: game.id,
+      playerOne: game.playerOne.name,
+      playerTwo: game.playerTwo.name,
+      createdAt: game.createdAt
+    }
   }
 
-  async getOne(id: number): Promise<Game | null> {
-    return await this.em.findOne(Game, { id }, { populate: ['playerOne', 'playerTwo'] })
+  async getOne(id: number): Promise<GameDto | null> {
+    // populate est utilisé pour charger les relations entre les entités
+    const game = await this.em.findOne(Game, { id }, { populate: ['playerOne', 'playerTwo'] })
+    if (!game) {
+      throw new NotFoundException(`Game with id ${id} not found`);
+    }
+    return {
+      id: game.id,
+      playerOne: game.playerOne.name,
+      playerTwo: game.playerTwo.name,
+      createdAt: game.createdAt
+    }
   }
 }
