f49298c935cb5d20cb8e66095cfe590af5581c3a
France Huon <france.huon@lonestone.io>
2025-02-24 11:49:29 +0100

    :hammer: updated player service and controller

--- apps/api/src/modules/player/player.controller.ts
+++ apps/api/src/modules/player/player.controller.ts
@@ -1,22 +1,22 @@
 import { Body, Controller, Get, Param, Post} from '@nestjs/common'
 import { PlayerService } from './player.service.js'
-import { Player } from '../../entities/player.entity.js'
+import { PlayerDto } from '@shifumi/dtos'
 
 @Controller('players')
 export class PlayerController {
   constructor(private playerService: PlayerService) {}
 
   @Post()
-  async create(@Body('name') name:string): Promise<Player> {
+  async create(@Body('name') name:string): Promise<PlayerDto> {
     return this.playerService.create(name)
   }
 
   @Get(':id')
-  async findById(@Param('id') id:number): Promise<Player> {
+  async findById(@Param('id') id:number): Promise<PlayerDto> {
     return this.playerService.findById(id)
   }
   @Get('name/:name')
-  async findByName(@Param('name') name:string): Promise<Player> {
+  async findByName(@Param('name') name:string): Promise<PlayerDto> {
     return this.playerService.findByName(name)
   }
 }
--- apps/api/src/modules/player/player.service.ts
+++ apps/api/src/modules/player/player.service.ts
@@ -1,12 +1,13 @@
 import { EntityManager } from '@mikro-orm/sqlite'
 import { Injectable, NotFoundException } from '@nestjs/common'
 import { Player } from '../../entities/player.entity.js'
+import { PlayerDto } from '@shifumi/dtos'
 
 @Injectable()
 export class PlayerService {
   constructor(private readonly em: EntityManager) {}
 
-  async create(name: string): Promise<Player> {
+  async create(name: string): Promise<PlayerDto> {
     return await this.em.transactional(async (em) => {
 
       let player = await this.em.findOne(Player, { name })
@@ -21,7 +22,7 @@ export class PlayerService {
     })
   }
 
-  async findById(id: number): Promise<Player> {
+  async findById(id: number): Promise<PlayerDto> {
     const player = await this.em.findOne(Player, { id });
     if (!player) {
       throw new NotFoundException(`Player with ID ${id} not found`)
@@ -29,7 +30,7 @@ export class PlayerService {
     return player
   }
 
-  async findByName(name: string): Promise<Player> {
+  async findByName(name: string): Promise<PlayerDto> {
     const player = await this.em.findOne(Player, { name });
     if (!player) {
       throw new NotFoundException(`Player with name ${name} not found`)
