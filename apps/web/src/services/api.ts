17d996110bd1a877c1b68424e03dff0e795fd559
France Huon <france.huon@lonestone.io>
2025-02-24 15:07:13 +0100

    :hammer: updated createGame function in api.ts

--- apps/web/src/services/api.ts
+++ apps/web/src/services/api.ts
@@ -1,25 +1,25 @@
+import type { CreateRoundDto, GameDto, PlayerDto, RoundDto } from '@shifumi/dtos'
 import { api } from '../config'
 
-export interface GameDto {
-  id: number
-  createdAt: Date
-  updatedAt: Date | null
-  playerOne: number
-  playerTwo: number
-}
-
-export async function createPlayer(name: string): Promise<{ id: number, name: string }> {
+// players
+export async function createPlayer(name: string): Promise<PlayerDto> {
   return await api.post('players', { json: { name } }).json()
 }
 
-export async function fetchOnePlayer(id: number): Promise<{ id: number, name: string }> {
-  return await api.get(`players/${id}`).json()
+export async function fetchOnePlayer(id: number): Promise<PlayerDto> {
+  return await api.get(`players/id/${id}`).json()
 }
 
-export async function fetchOnePlayerByName(name: string): Promise<{ id: number, name: string }> {
+export async function fetchOnePlayerByName(name: string): Promise<PlayerDto> {
   return await api.get(`players/name/${name}`).json()
 }
 
-export async function createGame(newGame: { playerTwoName: string }): Promise<GameDto> {
-  return await api.post('games', { json: newGame }).json()
+// game
+export async function createGame({ playerTwoName }: { playerTwoName: string }): Promise<GameDto> {
+  return await api.post('games', { json: { playerTwoName } }).json()
+}
+
+// round
+export async function createRound(createRoundDto: CreateRoundDto): Promise<RoundDto> {
+  return await api.post('rounds', { json: createRoundDto }).json()
 }
